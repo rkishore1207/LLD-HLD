35328b5e223f455e240694f14de85523b656f359
Kishore Ramesh <kishore.ramesh@kanini.com>
2024-05-23 23:16:05 +0530

    learned Simple Factory and Factory Method Design Pattern

---
+++ Projects/FactoryMethod/FactoryMethodDSP/Objects/Factories/DebugLogFactory.cs
@@ -0,0 +1,17 @@
+﻿using FactoryMethodDSP.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodDSP.Objects.Factories
+{
+    public class DebugLogFactory : IFactory
+    {
+        public ILogger CreateObject()
+        {
+            return new DebugLogger();
+        }
+    }
+}
---
+++ Projects/FactoryMethod/FactoryMethodDSP/Objects/InfoLogger.cs
@@ -0,0 +1,17 @@
+﻿using FactoryMethodDSP.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodDSP.Objects
+{
+    public class InfoLogger : ILogger
+    {
+        public void GetLogType()
+        {
+            Console.WriteLine("Created Info Log Object");
+        }
+    }
+}
