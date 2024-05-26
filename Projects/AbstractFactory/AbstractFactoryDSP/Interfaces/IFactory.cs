11da817df6858fd9a66651638ec9448cac98f128
Kishore Ramesh <kishore.ramesh@kanini.com>
2024-05-26 12:31:50 +0530

    learned Abstract factory design pattern

---
+++ Projects/AbstractFactory/AbstractFactoryDSP/Interfaces/IFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryDSP.Interfaces
+{
+    public interface IFactory
+    {
+        public IButton CreateButton();
+        public ITextBox CreateTextBox();
+    }
+}
---
+++ Projects/AbstractFactory/AbstractFactoryDSP/Interfaces/ITextBox.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryDSP.Interfaces
+{
+    public interface ITextBox
+    {
+        public void SetText();
+    }
+}
