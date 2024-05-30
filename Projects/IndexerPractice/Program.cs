123e232a258ed6af3a1ebd2e16415060464ab656
Kishore Ramesh <kishore.ramesh@kanini.com>
2024-05-30 10:39:57 +0530

    learned indexer in C#

---
+++ Projects/IndexerPractice/Program.cs
@@ -0,0 +1,17 @@
+﻿using static IndexerPractice.Indexer;
+
+namespace IndexerPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Company company = new Company();
+            // get value through indexer
+            Console.WriteLine( company[1] );
+            // set value throug indexer
+            company[2] = "Jadeja";
+            Console.WriteLine(company[2]);
+        }
+    }
+}
