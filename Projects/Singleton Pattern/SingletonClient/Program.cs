d590f4162aea9b93d639d0d308912270666995b0
Kishore Ramesh <kishore.ramesh@kanini.com>
2024-05-30 18:32:33 +0530

    learned singleton pattern

---
+++ Projects/Singleton Pattern/SingletonClient/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SingletonClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SingletonPattern obj1 = SingletonPattern.GetSingleton();
+            SingletonPattern obj2 = SingletonPattern.GetSingleton();
+            if (obj1 == obj2)
+                Console.WriteLine("Same Object");
+            else
+                Console.WriteLine("Different Object");
+        }
+    }
+}
---
+++ Projects/Singleton Pattern/SingletonClient/SingletonPattern.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonClient
+{
+    public class SingletonPattern
+    {
+        private SingletonPattern() { }
+        private static SingletonPattern instance;
+        public static SingletonPattern GetSingleton()
+        {
+            if (instance == null)
+                instance = new SingletonPattern();
+            return instance;
+        }
+    }
+}
