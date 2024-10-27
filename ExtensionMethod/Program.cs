585b0ba1fdfe3abea10e1164808c62c05b89ae5e
Kishore <146698138+rkishore1207@users.noreply.github.com>
2024-10-27 05:45:14 +0000

    Practiced Exension Meethods in C#

---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ExtensionMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "3Kishore";
+            Console.WriteLine(value.TransformFirstCharacter());
+        }
+    }
+}
