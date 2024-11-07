d252211d933295492b7b45ec7a4b61c3e93a2811
Kishore <146698138+rkishore1207@users.noreply.github.com>
2024-11-07 17:00:40 +0000

    Practiced toDictionary and toLookups

--- ExtensionMethod/Program.cs
+++ ExtensionMethod/Program.cs
@@ -15,6 +15,90 @@
             {
                 Console.WriteLine(country);
             }
+
+            //Dictionary
+            var dictionaries = new List<Student>()
+            {
+                new Student()
+                {
+                    Name = "Kishore",
+                    Age = 21,
+                    Country = "India",
+                    StudentId = 1
+                },
+                new Student()
+                {
+                    Name = "Mathan",
+                    Age = 22,
+                    Country = "America",
+                    StudentId = 2
+                },
+                new Student()
+                {
+                    Name = "Mahesh",
+                    Age = 23,
+                    Country = "China",
+                    StudentId = 3
+                },
+                new Student()
+                {
+                    Name = "Ajay",
+                    Age = 24,
+                    Country = "Japan",
+                    StudentId = 4
+                }
+            };
+
+            var dictionaryById = dictionaries.ToDictionary(student => student.StudentId);
+
+            foreach (var dictionary in dictionaryById)
+            {
+                Console.WriteLine(dictionary.Key + "\t" + dictionary.Value.Name);
+            }
+
+            //Lookups
+            var lookUps = new List<Student>()
+            {
+                new Student()
+                {
+                    Name = "Kishore",
+                    Age = 21,
+                    Country = "India",
+                    StudentId = 1
+                },
+                new Student()
+                {
+                    Name = "Mathan",
+                    Age = 22,
+                    Country = "India",
+                    StudentId = 2
+                },
+                new Student()
+                {
+                    Name = "Mahesh",
+                    Age = 23,
+                    Country = "Japan",
+                    StudentId = 3
+                },
+                new Student()
+                {
+                    Name = "Ajay",
+                    Age = 24,
+                    Country = "Japan",
+                    StudentId = 4
+                }
+            };
+
+            var lookUpByCountry = lookUps.ToLookup(student => student.Country);
+
+            foreach (var dictionary in lookUpByCountry)
+            {
+                Console.WriteLine(dictionary.Key);
+                foreach (var dictionary2 in lookUpByCountry[dictionary.Key])
+                {
+                    Console.WriteLine("\t"+dictionary2.Name);
+                }
+            }
         }
     }
 }
